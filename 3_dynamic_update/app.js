85a814bb0729bcde2da1f927d7416219eb351403
Jeroen Schalken <j.schalken@vanlanschotkempen.com>
2022-04-13 15:03:45 +0200

    Dynamic update demo added

--- 3_dynamic_update/app.js
+++ 3_dynamic_update/app.js
@@ -1,12 +1,39 @@
-var myData = [40, 10, 20, 60, 30];
+// https://www.d3indepth.com/datajoins/
 
-d3.select('.chart')
-  .selectAll('circle')
-  .data(myData)
-  .join('circle')
-  .attr('cx', function(d, i) {
-    return i * 100;
-  })
-  .attr('cy', 50)
-  .attr('r', 40)
-  .style('fill', 'orange');
+function getData() {
+    let data = [];
+    let numItems = Math.ceil(Math.random() * 5);
+  
+    for(let i=0; i<numItems; i++) {
+      data.push(Math.random() * 60);
+    }
+  
+    return data;
+  }
+  
+  function update(data) {
+    d3.select('.chart')
+      .selectAll('circle')
+      .data(data)
+      .join('circle')
+      .attr('cx', function(d, i) {
+        return 100 + (i * 100);
+      })
+      .attr('cy', 50)
+      .attr('r', function(d) {
+        return 0.9 * d;
+      })
+      .style('fill', function(d) {
+        return d > 30 ? 'orange' : '#999';
+      });
+  }
+  
+  function updateAll() {
+    let myData = getData();
+    update(myData);
+  }
+  
+  updateAll();
+  
+  d3.select("button")
+    .on("click", updateAll);
