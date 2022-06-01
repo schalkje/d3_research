7be67d7b6a5e682560111076e0ec87e0794c1ee7
Jeroen Schalken <j.schalken@vanlanschotkempen.com>
2022-06-01 16:05:40 +0200

    added cursor

--- src/features/database/Tables.js
+++ src/features/database/Tables.js
@@ -22,6 +22,7 @@ const Tables = () => {
       <h3>Table with columns</h3>
       <svg width="200" height="40" viewBox="0 0 100 20" className='table'>
         <rect style={{x:"0px",y:"0px",width: "100%", height:"100%"}}  className="background"/>
+        <rect style={{x:"2px",y:"2px",width: "96", height:"18"}}  className="header"/>
         <text x="5" y="14" width="100%" className="name">TableName</text>
       </svg>
 
@@ -41,7 +42,7 @@ const Tables = () => {
       </svg>
 
       <h3>With datatypes</h3>
-      <svg width="300" height="200" viewBox="0 0 150 100" className='table'>
+      <svg width="200" height="200" viewBox="0 0 150 100" className='table'>
         <rect style={{x:"0px",y:"0px",width: "100%", height:"100%"}}  className="background"/>
         <rect style={{x:"2px",y:"2px",width: "146", height:"18"}}  className="header"/>
         <rect style={{x:"2px",y:"18",width: "146", height:"80"}} className="body"/>
