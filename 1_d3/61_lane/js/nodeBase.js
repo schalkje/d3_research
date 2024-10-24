1e0bdca7ce480a8bbdbd9cc25060f135a18aff54
Jeroen Schalken <jeroen@schalken.com>
2024-10-24 09:17:22 +0200

    First lane renamed and placeholder lanes

---
+++ 1_d3/61_lane/js/nodeBase.js
@@ -0,0 +1,154 @@
+export default class BaseNode {
+  constructor(nodeData, parentElement, parentNode = null) {
+    this.data = nodeData;
+    this.parentElement = parentElement;
+    this.parentNode = parentNode;
+
+    this.id = nodeData.id;
+
+    this.element = null;
+    this.simulation = null;
+    this.layoutDebug = true;
+
+    if (!this.data.interactionState) this.data.interactionState = { expanded: true };
+
+    // Set default values for x, y, width, and height
+    if (!this.data.x) this.data.x = 0;
+    if (!this.data.y) this.data.y = 0;
+    if (!this.data.width) this.data.width = 60;
+    if (!this.data.height) this.data.height = 60;
+  }
+
+  renderContainer() {
+    console.log("Rendering Base Node renderContainer:", this.id, this.data.x, this.data.y, this.parentElement);
+    this.element = this.parentElement
+      .append("g")
+      .attr("width", this.data.width)
+      .attr("height", this.data.height)
+      .attr("class", "node")
+      .attr("data-id", this.id)
+      .on("click", (event) => {
+        console.log("Clicked on Adapter Node [BASE]:", this.id, event);
+        if (event) event.stopPropagation();
+        this.toggleExpandCollapse(this.element);
+      });
+    // .call(d3.drag()
+    //   .on("start", (event) => this.drag_started(event, this))
+    //   .on("drag", (event) => this.dragged(event, this))
+    //   .on("end", (event) => this.drag_ended(event, this)));
+
+    // show the center stip
+    if ( this.layoutDebug )
+      this.element.append("circle").attr("r", 3).attr("cx", 0).attr("cy", 0).attr("fill", "red");	
+
+    // Set expanded or collapsed state
+    if (this.data.interactionState.expanded) {
+      this.element.classed("expanded", true);
+    } else {
+      this.element.classed("collapsed", true);
+    }
+
+    return this.element;
+  }
+
+  render() {
+    renderContainer();
+  }
+
+  resize(size) {
+    const oldSize = {width: this.data.width, height: this.data.height};
+    this.data.width = size.width;
+    this.data.height = size.height;
+
+    this.data.x -= this.data.width / 2 - oldSize.width / 2;
+    this.data.y -= this.data.height / 2 - oldSize.height / 2;
+
+    this.element.attr("transform", `translate(${this.data.x}, ${this.data.y})`);
+  }
+
+  // Method to toggle expansion/collapse of the node
+  toggleExpandCollapse(container) {
+    this.data.interactionState.expanded = !this.data.interactionState.expanded;
+    this.updateLayout(container);
+  }
+
+  // Method to update the node rendering based on interaction state
+  updateLayout() {
+    console.log("    Updating Render for BASE:", this.id, this.data.interactionState.expanded);
+
+    if (this.data.interactionState.expanded) {
+      this.element.classed("collapsed", false).classed("expanded", true);
+    } else {
+      this.element.classed("expanded", false).classed("collapsed", true);
+    }
+  }
+
+  cascadeArrange() {
+    console.log("cascadeLayoutUpdate ", this.id);
+    if (this.parentNode) {
+      // this.parentNode.runSimulation();
+      this.parentNode.arrange();
+      this.parentNode.cascadeArrange();
+    }
+  }
+
+  cascadeRestartSimulation() {
+    console.log("cascadeRestartSimulation ", this.id);
+    if (this.simulation) {
+      console.log("                    simulaiton restart", this.id);
+      this.simulation.simulation.alphaTarget(0.8).restart();
+    }
+    if (this.parentNode) {
+      this.parentNode.cascadeRestartSimulation();
+    }
+  }
+
+  cascadeStopSimulation() {
+    console.log("cascadeStopSimulation ", this.id);
+    if (this.simulation) {
+      console.log("                    simulation stop", this.id);
+      this.simulation.simulation.alphaTarget(0);
+    }
+    if (this.parentNode) {
+      this.parentNode.cascadeStopSimulation();
+    }
+  }
+
+  getConnectionPoint() {}
+
+  // drag
+  drag_started(event, node) {
+    console.log("drag_started event", event, node);
+    // if (!d3.event.active) {
+    // Set the attenuation coefficient to simulate the node position movement process. The higher the value, the faster the movement. The value range is [0, 1]
+    // this.simulation.alphaTarget(0.8).restart()
+    // if (node.simulation) {
+    //   console.log("drag_started simulation",node.simulation);
+    //   // node.simulation.simulation.restart();
+    //   // node.runSimulation();
+    //   node.parentNode.cascadeRestartSimulation();
+    // }
+    // }
+    node.cascadeRestartSimulation();
+    event.fx = event.x;
+    event.fy = event.y;
+    node.element.classed("grabbing", true);
+  }
+
+  dragged(event, node) {
+    // console.log("dragged event",event);
+
+    event.fx = event.x;
+    event.fy = event.y;
+    // move the simulation
+    node.element.attr("transform", "translate(" + event.fx + "," + event.fy + ")");
+  }
+
+  drag_ended(event, node) {
+    // console.log("drag_ended event",event);
+
+    node.element.classed("grabbing", false);
+
+    node.cascadeStopSimulation();
+  }
+}
---
+++ 1_d3/61_lane/js/nodeBaseContainer.js
@@ -0,0 +1,221 @@
+import BaseNode from "./nodeBase.js";
+import Simulation from "./simulation.js";
+import { getComputedDimensions } from "./utils.js";
+
+export default class BaseContainerNode extends BaseNode {
+  constructor(nodeData, parentElement, parentNode = null) {
+    super(nodeData, parentElement, parentNode);
+    
+    this.simulation = null;
+    this.container = null;
+    this.containerMargin = { top: 18, right: 8, bottom: 8, left: 8 };
+    this.childNodes = [];
+  }
+
+  // Method to render the parent node and its children
+  async render() {
+    console.log(`Rendering BaseContainerNode type=${this.data.type} Node:`, this.id);
+    super.renderContainer();
+
+    // A container node consists of it's own display, a border, background and a label
+    // and a container where the node is rendered
+
+    // Append text to the top left corner of the element
+    const labelElement = this.element
+      .append("text")
+      .attr("x", -this.data.width / 2 + 4)
+      .attr("y", -this.data.height / 2 + 4)
+      .text(this.data.label)
+      .attr("class", `node label container ${this.data.type}`);
+      // .on("click", (event) => {
+      //   event.stopPropagation();
+      //   this.toggleExpandCollapse(this.element);
+      // });
+
+    this.minimumSize = getComputedDimensions(labelElement);
+    this.minimumSize.width += 8;
+    this.minimumSize.height += 4;
+    if (this.data.width < this.minimumSize.width || this.data.height < this.minimumSize.height) {
+      console.log("Render Resizing BaseContainerNode:", this.data.width, this.minimumSize.width, this.data.height, this.minimumSize.height);
+      this.data.width = Math.max(this.minimumSize.width, this.data.width);
+      this.data.height = Math.max(this.minimumSize.height, this.data.height);
+      // reposition the label based on the new size
+      labelElement.attr("x", -this.data.width / 2 + 4).attr("y", -this.data.height / 2 + 4);
+    }
+
+    // Draw the node shape
+    this.element
+      .append("rect")
+      .attr("class", (d) => `node shape container ${this.data.type}`)
+      .attr("width", this.data.width)
+      .attr("height", this.data.height)
+      .attr("x", -this.data.width / 2)
+      .attr("y", -this.data.height / 2)
+      .attr("rx", 5)
+      .attr("ry", 5);
+
+    if (this.data.interactionState.expanded) {
+      this.element.classed("expanded", true);
+      this.renderExpanded();
+    } else {
+      this.element.classed("collapsed", true);
+      this.renderCollapsed();
+    }
+
+    // you cannot move the g node,, move the child elements in stead
+    this.element.attr("transform", `translate(${this.data.x}, ${this.data.y})`);
+  }
+
+  resize(size) {
+    // make sure it doesn't go below minimum size
+    // console.log("BaseNodeContainer resize", size.width, this.minimumSize.width, size.height, this.minimumSize.height);
+    size.width = Math.max(size.width, this.minimumSize.width);
+    size.height = Math.max(size.height, this.minimumSize.height);
+
+    super.resize(size);
+
+    this.element
+      .select("rect")
+      .attr("width", this.data.width)
+      .attr("height", this.data.height)
+      .attr("x", -this.data.width / 2)
+      .attr("y", -this.data.height / 2);
+
+    this.element
+      .select("text")
+      .attr("x", -this.data.width / 2 + 4)
+      .attr("y", -this.data.height / 2 + 4);
+
+
+    if (this.container) {
+      const containerRect = getComputedDimensions(this.container);
+      const elementRect = getComputedDimensions(this.element);
+      const containerX = elementRect.x - containerRect.x + this.containerMargin.left;
+      const containerY = elementRect.y - containerRect.y + this.containerMargin.top;
+      // console.log(`                                        width: ${elementRect.x}-${containerRect.x}+${this.containerMargin.left}=${containerX}`);
+      // console.log(`                                       height: ${elementRect.y}-${containerRect.y}+${this.containerMargin.top}=${containerY}`);
+      this.container
+          .attr(
+            "transform",
+            `translate(${containerX}, ${containerY})`
+          );
+    }
+  }
+
+  // resize the node based on a resize of the container and it's child
+  resizeContainer(boundingBox) {
+    boundingBox.x -= this.containerMargin.left;
+    boundingBox.y -= this.containerMargin.top;
+    boundingBox.width += this.containerMargin.left + this.containerMargin.right;
+    boundingBox.height += this.containerMargin.top + this.containerMargin.bottom;
+
+    // make sure it doesn't go below minimum size
+    // console.log("ParentNode resize", boundingBox.width, this.minimumSize.width, boundingBox.height, this.minimumSize.height);
+    boundingBox.width = Math.max(boundingBox.width, this.minimumSize.width);
+    boundingBox.height = Math.max(boundingBox.height, this.minimumSize.height);
+
+    this.resize(boundingBox);
+  }
+
+  async renderExpanded() {
+    // restore the expanded size if it was stored
+    if (this.data.expandedSize) {
+      this.data.height = this.data.expandedSize.height;
+      this.data.width = this.data.expandedSize.width;
+    }
+
+    // this.resize({ width: this.data.width, height: this.data.height });
+    // this.element.select("rect").attr("width", this.data.width).attr("height", this.data.height);
+
+    const containerWidth = this.data.width - this.containerMargin.left - this.containerMargin.right;
+    const containerHeight = this.data.height - this.containerMargin.top - this.containerMargin.bottom;
+    this.container = this.element
+      .append("g")
+      .attr("class", (d) => `node container parent`);
+
+
+    // Set expanded or collapsed state
+    await this.renderChildren();
+
+    this.resize({ width: this.data.width, height: this.data.height });
+  }
+
+  async renderCollapsed() {
+    // store the expanded size before collapsing
+    if (this.data.height > this.minimumSize.height || this.data.width > this.minimumSize.width)
+      this.data.expandedSize = { height: this.data.height, width: this.data.width };
+
+    // set the collapsed size
+    this.data.height = this.minimumSize.height;
+    this.data.width = this.minimumSize.width;
+
+    // apply the collapsed size to the rectangle
+    this.element.select("rect").attr("width", this.data.width).attr("height", this.data.height);
+
+    this.resize({ width: this.data.width, height: this.data.height });
+
+    // this.cascadeLayoutUpdate(); // JS: todo: ugly: why do we need to call this here (and not in the rederExpanded method)?
+  }
+
+  // Method to toggle expansion/collapse of the parent node
+  toggleExpandCollapse() {
+    this.data.interactionState.expanded = !this.data.interactionState.expanded;
+    this.updateLayout();
+  }
+
+  // Method to update rendering based on interaction state
+  updateLayout() {
+    if (this.data.interactionState.expanded) {
+      this.element.classed("collapsed", false).classed("expanded", true);
+      this.renderExpanded();
+    } else {
+      this.element.classed("expanded", false).classed("collapsed", true);
+      this.removeChildren();
+      this.renderCollapsed();
+    }
+
+    this.cascadeArrange();
+  }
+
+  arrange() {
+    console.log("Arranging BaseContainerNode:", this.id);
+  }
+
+  async renderChildren() {
+    console.log("    Rendering Children for BaseContainer:", this.id, this.data.children);
+    // no default rendering of the children, but this renders a placeholder rect
+    const containerWidth = this.data.width - this.containerMargin.left - this.containerMargin.right;
+    const containerHeight = this.data.height - this.containerMargin.top - this.containerMargin.bottom;
+    this.container
+      .append("rect")
+      .attr("class", (d) => `node placeholder`)
+      .attr("width", containerWidth)
+      .attr("height", containerHeight)
+      .attr("fill", "red")
+      .attr("stroke", "red")
+      .attr("stroke-width", 2)
+      .attr("x", -containerWidth / 2)
+      .attr("y", -containerHeight / 2);
+
+    if ( this.layoutDebug )
+      this.container.append("circle").attr("r", 3).attr("cx", 0).attr("cy", 0).attr("fill", "red");	
+  
+
+    this.container.attr(
+      "transform",
+      `translate(
+            ${this.containerMargin.left - this.containerMargin.right}, 
+            ${this.containerMargin.top- this.containerMargin.bottom}
+          )`
+    );
+}
+
+  // Method to remove child nodes from the SVG
+  removeChildren() {
+    console.log("    Removing Children for Parent:", this.id);
+    this.container.selectAll("*").remove();
+    this.container.remove();
+    this.childNodes = [];
+  }
+}
+
